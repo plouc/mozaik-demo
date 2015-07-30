a7e3d2a48393ff558ec16bf144af5622ca975bad
Raphaël Benitte <raphael.benitte@fullsix.com>
2015-07-30 07:19:45 +0200

    Add time & weather extensions

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Mozaik            from 'mozaik/browser';
 import githubComponents  from 'mozaik-ext-github';
 //import herokuComponents  from 'mozaik-ext-heroku';
 import travisComponents  from 'mozaik-ext-travis';
-//import timeComponents    from 'mozaik-ext-time';
-//import weatherComponents from 'mozaik-ext-weather';
+import timeComponents    from 'mozaik-ext-time';
+import weatherComponents from 'mozaik-ext-weather';
+
 
 const MozaikComponent = Mozaik.Component.Mozaik;
 const ConfigActions   = Mozaik.Actions.Config;
+
 
 Mozaik.Registry.addExtensions({
     github:  githubComponents,
     //heroku:  herokuComponents,
     travis:  travisComponents,
-    //time:    timeComponents,
-    //weather: weatherComponents
+    time:    timeComponents,
+    weather: weatherComponents
 });
 
 React.render(<MozaikComponent/>, document.getElementById('mozaik'));
