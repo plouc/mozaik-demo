2ceb5dc087376fcebf19c6f44233b92e07ac82a4
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-07-15 13:50:41 +0900

    feat(themes): add official mozaik themes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
-import { render } from 'react-dom'
-import Mozaik, { Registry } from '@mozaik/ui'
+import ReactDOM from 'react-dom'
+import Mozaik, { Registry, ThemeManager } from '@mozaik/ui'
 
+// themes
+import {
+    miniKuroTheme,
+    nightBlueTheme,
+    solarizedDarkTheme,
+    wineTheme,
+} from '@mozaik/themes'
+
+// extensions
 import github from '@mozaik/ext-github'
 import travis from '@mozaik/ext-travis'
+
+ThemeManager.add(miniKuroTheme)
+ThemeManager.add(nightBlueTheme)
+ThemeManager.add(solarizedDarkTheme)
+ThemeManager.add(wineTheme)
+
+ThemeManager.defaultTheme = solarizedDarkTheme.name
 
 Registry.addExtensions({
     github,
     travis,
 })
 
-render(<Mozaik />, document.getElementById('mozaik'))
+ReactDOM.render(<Mozaik />, document.getElementById('mozaik'))
