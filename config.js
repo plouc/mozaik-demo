73af309dcf61cd2f49825fbddaf2ebf1509a7854
Valerio Sillari <valerio.sillari@gmail.com>
2017-12-20 14:59:36 +0100

    now we got only one dashboard. no rotating panels

--- config.js
+++ config.js
@@ -38,74 +38,7 @@ var config = {
     apisPollInterval: 15000,
 
     dashboards: [
-
-        // first dashboard
-        {
-            // 4 x 3 dashboard
-            columns: 4,
-            rows:    3,
-            widgets: [
-                {
-                    type: 'github.user_badge',
-                    user: 'plouc',
-                    columns: 1, rows: 1,
-                    x: 0, y: 0
-                },
-                {
-                    type: 'github.repository_contributors_stats',
-                    repository: 'plouc/mozaik',
-                    columns: 1, rows: 1,
-                    x: 2, y: 0
-                },
-                {
-                    type: 'travis.repository',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 1, rows: 1,
-                    x: 1, y: 0
-                },
-                {
-                    type: 'travis.build_histogram',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 2, rows: 1,
-                    x: 1, y: 1
-                },
-                {
-                    type: 'time.clock',
-                    columns: 1, rows: 1,
-                    x: 3, y: 0
-                },
-                {
-                    type: 'travis.repository',
-                    owner: 'plouc',
-                    repository: 'mozaik-demo',
-                    columns: 1, rows: 1,
-                    x: 0, y: 1
-                },
-                {
-                    type: 'travis.build_history',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 1, rows: 2,
-                    x: 3, y: 1
-                },
-                {
-                    type: 'travis.build_histogram',
-                    owner: 'plouc',
-                    repository: 'go-gitlab-client',
-                    columns: 2, rows: 1,
-                    x: 1, y: 2
-                },
-                {
-                    type: 'github.status',
-                    columns: 1, rows: 1,
-                    x: 0, y: 2
-                }
-            ]
-        },
-
-        // second dashboard
+        // dashboard
         {
             // 3 x 2 dashboard
             columns: 3,
